e6b42ca4ba2ed49a55acd3f119bfd779d9d19dce
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2023-04-04 22:15:40 -0400

    fix

--- src/components/tagline.js
+++ src/components/tagline.js
@@ -20,59 +20,65 @@ const Tagline = () => {
   }
   let PageComponent = () => null;
   if (page === 0) {
-    PageComponent = () => (<section className="grid-container usa-section usa-prose" id="survey-start">
-      <center>
-        <StepIndicator counters="default" headingLevel="h4">
-          {stages.map((label, i) => (
-            <StepIndicatorStep
-              label={label}
-              status={i == page ? 'current' : i < page ? 'complete' : ''}
-            />
-          ))}
-        </StepIndicator>
-        <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
-          Get Started with CyberCare
-        </h2>
-        <p>
-          To sign up your organization for CyberCare, fill out basic information
-          and complete the CPG self-assessment below to receive your starter
-          bundle.
-        </p>
-        <Form schema={{
-          type: "object",
-          properties: {
-            name: { type: "string", title: "Organization name" },
-            size: { type: "string", title: "Organization size" },
-            contactName: { type: "string", title: "Contact name" },
-            email: { type: "string", title: "Email", format: "email" },
-            itStaff: { type: "number", title: "How many people are part of your full-time IT staff? Do you have one?" },
-            servicesInUse: {
-              type: "string",
-              title: "What cybersecurity services do you currently use?",
-              enum: ["Cyber Hygiene", "Vulnerability scanning"]
-              // type: "array", title: "What cybersecurity services do you currently use?", items: {
-              //   type: "string",
-              //   enum: ["Cyber Hygiene", "Vulnerability scanning"]
-              // }, uniqueItems: true
-            },
-            biggestProblem: { type: "string", title: "What is your biggest challenge around cybersecurity?", enum: ["Challenge 1", "Challenge 2", "Challenge 3"] }
+    PageComponent = () => (<>
+      <Form schema={{
+        type: "object",
+        properties: {
+          name: { type: "string", title: "Organization name" },
+          size: { type: "string", title: "Organization size" },
+          contactName: { type: "string", title: "Contact name" },
+          email: { type: "string", title: "Email", format: "email" },
+          itStaff: { type: "number", title: "How many people are part of your full-time IT staff? Do you have one?" },
+          servicesInUse: {
+            type: "string",
+            title: "What cybersecurity services do you currently use?",
+            enum: ["Cyber Hygiene", "Vulnerability scanning"]
+            // type: "array", title: "What cybersecurity services do you currently use?", items: {
+            //   type: "string",
+            //   enum: ["Cyber Hygiene", "Vulnerability scanning"]
+            // }, uniqueItems: true
+          },
+          biggestProblem: { type: "string", title: "What is your biggest challenge around cybersecurity?", enum: ["Challenge 1", "Challenge 2", "Challenge 3"] }
+        }
+      }}
+        uiSchema={{
+          servicesInUse: {
+            // "ui:widget": "checkboxes"
           }
         }}
-          uiSchema={{
-            servicesInUse: {
-              // "ui:widget": "checkboxes"
-            }
-          }}
-        >&nbsp;</Form>
-      </center>
-    </section>);
+      >&nbsp;</Form></>);
+  } if (page === 1) {
+    PageComponent = () => (
+      <div>test</div>
+    );
+  } if (page === 3) {
+    PageComponent = () => (
+      <p>You're done!</p>
+    )
   }
 
-  return (<>
-    <PageComponent />
-    <button onClick={() => previousPage()}>Previous</button>
-    <button onClick={() => nextPage()}>Next</button>
-    </>);
+  return (<section className="grid-container usa-section usa-prose" id="survey-start">
+    <center>
+      <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+        Get Started with CyberCare
+      </h2>
+      <p>
+        To sign up your organization for CyberCare, fill out basic information
+        and complete the CPG self-assessment below to receive your starter
+        bundle.
+      </p>
+      <StepIndicator counters="default" headingLevel="h4">
+        {stages.map((label, i) => (
+          <StepIndicatorStep
+            label={label}
+            status={i == page ? 'current' : i < page ? 'complete' : ''}
+          />
+        ))}
+      </StepIndicator>
+      <PageComponent />
+      <button onClick={() => previousPage()} disabled={page - 1 < 0}>Previous</button>
+      <button onClick={() => nextPage()} disabled={page + 1 >= NUM_PAGES}>Next</button>
+    </center></section>);
 }
 
 export default Tagline;
