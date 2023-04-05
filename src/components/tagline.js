28f83309d2cf7fafa4c12231f5f7aba03790208d
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2023-04-04 22:57:19 -0400

    update

--- src/components/tagline.js
+++ src/components/tagline.js
@@ -10,6 +10,248 @@ let stages = ['Your Organization', 'Maturity Assessment', 'Get Your CyberCare'];
 
 const NUM_PAGES = stages.length;
 
+const schema = {
+  type: "object",
+  definitions: {
+    "cpg": {
+      type: "object",
+      properties: {
+        status: {
+          type: "string",
+          enum: ["Implemented", "In Progress", "Scoped", "Not Started"]
+        },
+        notes: {
+          type: "string"
+        }
+      }
+    }
+  },
+  properties: {
+    type: "object",
+    "1": {
+      "title": "Identify (1)",
+      type: "object",
+      properties: {
+        "a": {
+          title: "Do you maintain a regularly updated inventory of all organizational assets with an IP address, and update this on a recurring basis?",
+          "$ref": "#/definitions/cpg",
+        },
+        "b": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "c": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "d": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "e": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "f": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "g": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "h": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "i": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        }
+      }
+    },
+    "2": {
+      "title": "Protect (2)",
+      type: "object",
+      properties: {
+        "a": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "b": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "c": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "d": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "e": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "f": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "g": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "h": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "i": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "j": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "k": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "l": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "m": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "n": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "o": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "p": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "q": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "r": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "s": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "t": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "u": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "v": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "w": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "x": {
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        }
+      }
+    },
+    "3": {
+      "title": "Detect (3)",
+      type: "object",
+      properties: {
+        "a": {
+          type: "string",
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        }
+      }
+    },
+    "4": {
+      "title": "Respond (4)",
+      type: "object",
+      properties: {
+        "a": {
+          type: "string",
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "b": {
+          type: "string",
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        },
+        "c": {
+          type: "string",
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        }
+      }
+    },
+    "5": {
+      "title": "Recover (5)",
+      type: "object",
+      properties: {
+        "a": {
+          type: "string",
+          title: "Asset Inventory",
+          "$ref": "#/definitions/cpg",
+        }
+      }
+    }
+  }
+};
+
+let uiSchema = {
+  1: {
+    classNames: "cpg cpg-1"
+  },
+  2: {
+    classNames: "cpg cpg-2"
+  },
+  3: {
+    classNames: "cpg cpg-3"
+  },
+  4: {
+    classNames: "cpg cpg-4"
+  },
+  5: {
+    classNames: "cpg cpg-5"
+  },
+};
+
+for (const i in uiSchema) {
+  console.log(i);
+  for (const key of Object.keys(schema.properties[i].properties)) {
+    uiSchema[i][key] = {
+      status: {
+        "ui:title": " ",
+        "ui:placeholder": "Select a status"
+      },
+      notes: {
+        "ui:title": " ",
+        "ui:placeholder": "Notes"
+      }
+    };
+  }
+}
+
 const Tagline = () => {
   const [page, setPage] = useState(1);
   const nextPage = () => {
@@ -49,255 +291,9 @@ const Tagline = () => {
       >&nbsp;</Form></>);
   } if (page === 1) {
     PageComponent = () => (<>
-      <Form schema={{
-        type: "object",
-        definitions: {
-          "cpg": {
-            type: "string"
-          }
-        },
-        properties: {
-          type: "object",
-          "1": {
-            "title": "Identify (1)",
-            type: "object",
-            properties: {
-              "a": {
-                type: "string",
-                title: "Do you maintain a regularly updated inventory of all organizational assets with an IP address, and update this on a recurring basis?",
-                "$ref": "#/definitions/cpg",
-              },
-              "b": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "c": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "d": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "e": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "f": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "g": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "h": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "i": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              }
-            }
-          },
-          "2": {
-            "title": "Protect (2)",
-            type: "object",
-            properties: {
-              "a": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "b": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "c": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "d": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "e": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "f": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "g": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "h": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "i": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "j": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "k": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "l": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "m": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "n": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "o": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "p": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "q": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "r": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "s": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "t": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "u": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "v": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "w": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              },
-              "x": {
-                type: "string",
-                title: "Asset Inventory",
-                "$ref": "#/definitions/cpg",
-              }
-            },
-            "3": {
-              "title": "Detect (3)",
-              type: "object",
-              properties: {
-                "a": {
-                  type: "string",
-                  title: "Asset Inventory",
-                  "$ref": "#/definitions/cpg",
-                }
-              }
-            },
-            "4": {
-              "title": "Respond (4)",
-              type: "object",
-              properties: {
-                "a": {
-                  type: "string",
-                  title: "Asset Inventory",
-                  "$ref": "#/definitions/cpg",
-                },
-                "b": {
-                  type: "string",
-                  title: "Asset Inventory",
-                  "$ref": "#/definitions/cpg",
-                },
-                "c": {
-                  type: "string",
-                  title: "Asset Inventory",
-                  "$ref": "#/definitions/cpg",
-                }
-              }
-            },
-            "5": {
-              "title": "Recover (5)",
-              type: "object",
-              properties: {
-                "a": {
-                  type: "string",
-                  title: "Asset Inventory",
-                  "$ref": "#/definitions/cpg",
-                }
-              }
-            }
-          }
-        }
-      }}
+      <Form schema={schema}
       className="cpg-form"
-      uiSchema={{
-        1: {
-          classNames: "cpg cpg-1"
-        },
-        2: {
-          classNames: "cpg cpg-2"
-        },
-        3: {
-          classNames: "cpg cpg-3"
-        },
-        4: {
-          classNames: "cpg cpg-4"
-        },
-        5: {
-          classNames: "cpg cpg-5"
-        },
-      }}
+      uiSchema={uiSchema}
       >&nbsp;</Form></>);
   } if (page === 2) {
     PageComponent = () => (
